d8334c2555c79cd8b0f986901b51775b83508924
Julien Thevenon <julien_thevenon@yahoo.fake.fr>
2015-09-29 12:04:01 +0200

    Improve size of output file by using differential pictures

--- include/gif_creator.h
+++ include/gif_creator.h
@@ -36,7 +36,10 @@ namespace gif_creator
     inline gif_creator(const std::string & p_file_name);
     inline ~gif_creator(void);
   private:
-    std::vector<lib_bmp::my_bmp *> m_bmp_files;
+    static inline const unsigned int & convert_to_gif_color(const lib_bmp::my_color_alpha & p_bmp_color,
+							    const std::map<lib_bmp::my_color_alpha,unsigned int> & p_conversion_table);
+
+    std::vector<std::pair<std::string,lib_bmp::my_bmp *> > m_bmp_files;
   };
 
 
@@ -47,7 +50,7 @@ namespace gif_creator
     size_t l_pattern_pos = p_file_name.find("#");
     if(std::string::npos == l_pattern_pos) throw quicky_exception::quicky_logic_exception("Failed to find '#' character in file patter \"" + p_file_name + "\"",__LINE__,__FILE__);
 
-
+    // Search files matching pattern
     std::vector<std::string> l_file_list;
     bool l_continu = true;
     unsigned int l_file_index = 0;
@@ -75,21 +78,23 @@ namespace gif_creator
       }
     while(l_continu);
 
+    if(!l_file_list.size())
+      {
+	throw quicky_exception::quicky_logic_exception("No file found matching pattern \"" + p_file_name + "\"",__LINE__,__FILE__);
+      }
+
+    // Iterator on files to insure they have coherent dimensions, palette size and store them in memory
     std::set<lib_bmp::my_color_alpha> l_unified_bmp_palette;
     unsigned int l_width = 0;
     unsigned int l_height = 0;
     for(auto l_file_iter : l_file_list)
       {
+	std::cout << "Load file \"" << l_file_iter << "\"" << std::endl ;
 	// Load BMP files
 	lib_bmp::my_bmp *l_bmp = new lib_bmp::my_bmp(l_file_iter);
-	m_bmp_files.push_back(l_bmp);
+	m_bmp_files.push_back(std::pair<std::string,lib_bmp::my_bmp *>(l_file_iter,l_bmp));
 
-	// DIsplay BMP information
-	//l_bmp->display_info();
-	std::cout << "Image width : " << l_bmp->get_width() << std::endl ;
-	std::cout << "Image height : " << l_bmp->get_height() << std::endl ;
-	std::cout << "Nb bits per pixel = " << l_bmp->get_nb_bits_per_pixel() << std::endl ;
-
+	// Store Picuture dimensions
 	if(!l_width && ! l_height)
 	  {
 	    l_width = l_bmp->get_width();
@@ -146,28 +151,93 @@ namespace gif_creator
 
     l_gif.activate_loop(0);
 
-    // Convert each BMP file into a GIF picture
-    for(auto l_bmp_iter : m_bmp_files)
+    // Directly Convert first BMP file into a GIF picture
+    lib_bmp::my_bmp *l_bmp = m_bmp_files.front().second;
+    lib_gif::gif_image * l_gif_image = new lib_gif::gif_image(l_width,l_height);
+    for(unsigned int l_y = 0 ; l_y < l_height ; ++l_y)
       {
-	l_gif.add_graphic_control_extension(100,false);
+	for(unsigned int l_x = 0 ; l_x < l_width ; ++l_x)
+	  {
+	    l_gif_image->set_color_index(l_x,l_y,convert_to_gif_color(l_bmp->get_pixel_color(l_x,l_y),l_conversion_colors));
+	  }
+      }
+    l_gif.add_graphic_control_extension(100,false);
+    l_gif.add_image(*l_gif_image);
 
-	lib_gif::gif_image * l_gif_image = new lib_gif::gif_image(l_width,l_height);
+    lib_bmp::my_bmp * l_previous_bmp_file = l_bmp;
+    // For other files convert only differences to a GIF picture
+    for(unsigned int l_bmp_index = 1 ; l_bmp_index < m_bmp_files.size() ; ++l_bmp_index)
+      {
+	std::cout << "Converting file \"" << m_bmp_files[l_bmp_index].first << std::endl ;
+
+	l_bmp = m_bmp_files[l_bmp_index].second;
+
+	// Compute differences with previous picture
+	std::set<unsigned int> l_color_difference_index;
+	std::map<std::pair<unsigned int,unsigned int>, unsigned int> m_differences;
+	unsigned int l_min_x_diff = l_width;
+	unsigned int l_max_x_diff = 0;
+	unsigned int l_min_y_diff = l_height;
+	unsigned int l_max_y_diff = 0;
 	for(unsigned int l_y = 0 ; l_y < l_height ; ++l_y)
 	  {
 	    for(unsigned int l_x = 0 ; l_x < l_width ; ++l_x)
 	      {
-		const lib_bmp::my_color_alpha & l_bmp_color = l_bmp_iter->get_pixel_color(l_x,l_y);
-		std::map<lib_bmp::my_color_alpha,unsigned int>::const_iterator l_color_iter = l_conversion_colors.find(l_bmp_color);
-		if(l_conversion_colors.end() == l_color_iter)
+		const lib_bmp::my_color_alpha & l_bmp_color = l_bmp->get_pixel_color(l_x,l_y);
+		const lib_bmp::my_color_alpha & l_previous_bmp_color = l_previous_bmp_file->get_pixel_color(l_x,l_y);
+		// Check if pixel has changed
+		if(l_previous_bmp_color != l_bmp_color)
 		  {
-		    std::stringstream l_color_stream;
-		    l_color_stream << "R" << std::setw(3) << (uint32_t)l_bmp_color.get_red() << ", G=" << std::setw(3) << (uint32_t)l_bmp_color.get_green() << ", B=" << std::setw(3) << (uint32_t)l_bmp_color.get_blue();
-		    throw quicky_exception::quicky_logic_exception("BMP color \"" + l_color_stream.str() + " not found insigned unified GIF palette",__LINE__,__FILE__);
+		    // Store coordinates if they are extrema
+		    if(l_min_x_diff > l_x)
+		      {
+			l_min_x_diff = l_x;
+		      }
+		    if(l_min_y_diff > l_y)
+		      {
+			l_min_y_diff = l_y;
+		      }
+		    if(l_max_x_diff < l_x)
+		      {
+			l_max_x_diff = l_x;
+		      }
+		    if(l_max_y_diff < l_y)
+		      {
+			l_max_y_diff = l_y;
+		      }
+		    unsigned int l_gif_color_index = convert_to_gif_color(l_bmp_color,l_conversion_colors);
+		    l_color_difference_index.insert(l_gif_color_index);
+		    m_differences.insert(std::map<std::pair<unsigned int,unsigned int>, unsigned int>::value_type(std::pair<unsigned int,unsigned int>(l_x,l_y),l_gif_color_index));
 		  }
-		l_gif_image->set_color_index(l_x,l_y,l_color_iter->second);
+	      }
+	  }
+
+	// Compute transparent index
+	unsigned int l_transparent_index = 0;
+	while(l_transparent_index < 256 && l_color_difference_index.end() != l_color_difference_index.find(l_transparent_index))
+	  {
+	    ++l_transparent_index;
+	  }
+	assert(l_transparent_index < 256);
+	std::cout << "Transparent index : " << l_transparent_index << "\tDiff window (" << l_min_x_diff << "," << l_min_y_diff << ")-(" << l_max_x_diff << "," << l_max_y_diff << ")" << std::endl ;
+
+	l_gif.add_graphic_control_extension(100,true,l_transparent_index);
+
+	unsigned int l_diff_width = l_max_x_diff - l_min_x_diff + 1;
+	unsigned int l_diff_heigth = l_max_y_diff - l_min_y_diff + 1;
+	l_gif_image = new lib_gif::gif_image(l_diff_width,l_diff_heigth,l_min_x_diff,l_min_y_diff);
+
+	// Generate differential picture
+	for(unsigned int l_y = 0 ; l_y < l_diff_heigth ; ++l_y)
+	  {
+	    for(unsigned int l_x = 0 ; l_x < l_diff_width ; ++l_x)
+	      {
+		std::map<std::pair<unsigned int,unsigned int>, unsigned int>::const_iterator l_pixel_iter = m_differences.find(std::pair<unsigned int,unsigned int>(l_x + l_min_x_diff,l_y + l_min_y_diff));
+		l_gif_image->set_color_index(l_x,l_y,m_differences.end() == l_pixel_iter ? l_transparent_index : l_pixel_iter->second);
 	      }
 	  }
 	l_gif.add_image(*l_gif_image);
+	l_previous_bmp_file = l_bmp;
       }
 
     // Determine output name
@@ -203,9 +273,24 @@ namespace gif_creator
   {
     for(auto l_iter: m_bmp_files)
       {
-	delete l_iter;
+	delete l_iter.second;
       }
   }
+
+  //----------------------------------------------------------------------------
+  const unsigned int & gif_creator::convert_to_gif_color(const lib_bmp::my_color_alpha & p_bmp_color,
+							 const std::map<lib_bmp::my_color_alpha,unsigned int> & p_conversion_table)
+  {
+    std::map<lib_bmp::my_color_alpha,unsigned int>::const_iterator l_color_iter = p_conversion_table.find(p_bmp_color);
+    if(p_conversion_table.end() != l_color_iter)
+      {
+	return l_color_iter->second;
+      }
+    std::stringstream l_color_stream;
+    l_color_stream << p_bmp_color;
+    throw quicky_exception::quicky_logic_exception("BMP color \"" + l_color_stream.str() + " not found insigned unified GIF palette",__LINE__,__FILE__);
+  }
+
 }
 #endif // GIF_CREATOR_H
 //EOF
